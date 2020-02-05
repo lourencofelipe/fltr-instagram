cd6594acacc819f5ab2a2b0291b4e406363f564a
lourencofelipe <felipe.pucsc@gmail.com>
2020-02-05 08:53:07 -0300

    creating project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:fltr_instagram/pages/feed.pages.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: FeedPage());
+  }
+}
