ca7ec90debaada57c5d1b13978d9b0d6c9459c58
lourencofelipe <felipe.pucsc@gmail.com>
2020-02-11 08:39:44 -0300

    Creating stories

--- lib/pages/insta_list.pages.dart
+++ lib/pages/insta_list.pages.dart
@@ -1,9 +1,15 @@
+import 'package:fltr_instagram/pages/insta_stories.pages.dart';
 import 'package:flutter/material.dart';
 
 class InstaList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return new ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => index == 0
+      ? new SizedBox(
+        child: new InstaStories(),
+      ):Column(),
     );
   }
 }
--- lib/pages/insta_stories.pages.dart
+++ lib/pages/insta_stories.pages.dart
@@ -1,9 +1,42 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class InstaStories extends StatelessWidget {
+
+  final topText = Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: <Widget>[
+      Text(
+        "Stories",
+        style: new TextStyle(fontWeight: FontWeight.bold),
+      ),
+     new Row(
+       children: <Widget>[
+         new Icon(Icons.play_arrow),
+         new Text(
+           "Watch All", 
+           style: TextStyle(
+             fontWeight: FontWeight.bold),
+          ),
+       ],
+     )
+    ],
+  );
+
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: const EdgeInsets.all(16.0),
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          topText,
+          //stories,
+        ],
+      ),
     );
   }
 }
